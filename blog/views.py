b9553512dd9b24b662f5053a1cfd759eaa3f9748
dnjsrywn11 <dnjsrywn@gmail.com>
2022-12-06 07:02:17 +0000

    CBV detail page update

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,43 @@
-from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def index(request):
     
-    posts =Post.objects.all().order_by('-pk')
-    
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
+    # template_name = 'blog/index.html'
 
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
+
+# from django.shortcuts import render
+# from .models import Post
+
+
+
+# def index(request):
     
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#     posts =Post.objects.all().order_by('-pk')
+    
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+
+# def single_post_page(request,pk):
+#     post = Post.objects.get(pk=pk)
+    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
 # Create your views here.
