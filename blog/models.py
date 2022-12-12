7ad368bbb4d965fb1739b9a5ab182302d45783a5
dnjsrywn11 <dnjsrywn@gmail.com>
2022-12-12 06:24:08 +0000

    list-detail tag

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,17 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -33,6 +43,7 @@ class Post(models.Model):
     
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     
+    tags = models.ManyToManyField(Tag, blank=True)
     
     def __str__(self):
         return f'[{self.pk}]{self.title}::{self.author}'
