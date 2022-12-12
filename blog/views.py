ba4b693cd0b8f8af9146cf7a8f7aaa69e8a5a24b
dnjsrywn11 <dnjsrywn@gmail.com>
2022-12-12 03:32:52 +0000

    Categroy pages maked

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ class PostDetail(DetailView):
     
 def category_page(request,slug):
     if slug == 'no_category':
-        category = "비분류"
+        category = "미분류"
         post_list = Post.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
@@ -36,10 +36,10 @@ def category_page(request,slug):
         request,
         'blog/post_list.html',
         {
-            'post_list':post_list,
-            'categories':Category.objects.all(),
-            'no_category_post_count':Post.objects.filter(category=None).count(),
-            'category':category,
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
         }
      )
         
