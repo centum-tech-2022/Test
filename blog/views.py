db56e01da325a2c13b32afd0f91edcdf0e912516
dnjsrywn11 <dnjsrywn@gmail.com>
2022-12-19 07:45:12 +0000

    lannding page update

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,12 @@ from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
 from .forms import CommentForm
+from django.db.models import Q
+
+    
+
+
+
 
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
@@ -106,6 +112,7 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
     
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -192,6 +199,26 @@ def delete_comment(request, pk):
         raise PermissionDenied
     
 
+    
+    
+class PostSearch(PostList):
+    paginate_by = None
+    
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        
+        return context
+    
+    
 # template_name = 'blog/index.html'
 
 
