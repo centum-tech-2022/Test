69be724e6300716f974317c2798cadd2c76ebc05
dnjsrywn11 <dnjsrywn@gmail.com>
2022-12-19 02:25:43 +0000

    comment delete

--- blog/views.py
+++ blog/views.py
@@ -180,7 +180,19 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
     
-    # template_name = 'blog/index.html'
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+    
+
+# template_name = 'blog/index.html'
 
 
 # from django.shortcuts import render
